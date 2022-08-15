370c40224e3e1d62d60a959495d2ffb57a6fdfb1
kstetsyuk <stetsyuk.karina@epa.gov>
2022-08-15 15:05:49 -0400

    Updated comments to R scripts

--- R/bulk_data_api_demo.R
+++ R/bulk_data_api_demo.R
@@ -4,20 +4,25 @@ library(jsonlite)
 
 # API info
 apiUrlBase <- "https://api.epa.gov/easey/dev"
-apiKEY <- "YOUR_API_KEY"
+apiKEY <- "B9jNE2eT6xiWOANh0jjeHcBKbZvyD7P3gIgARghb"
 
-# S3 bucket url base + s3Path in get request is the full path to the files
+# base to S3 bucket
 bucketUrlBase <- 'https://api.epa.gov/easey/dev/bulk-files/'
 
-# annual emissions url
+# bulk data files url
 servicesUrl <- paste0(apiUrlBase,"/camd-services/bulk-files?API_KEY=",apiKEY)
 
+# API GET request
 res = GET(servicesUrl)
 
+# convert response to a data frame
 bulkFiles <- fromJSON(rawToChar(res$content))
 
+# filter by compliance data type
 complianceFiles <- bulkFiles[bulkFiles$metadata$dataType=="Compliance",]
 
+# filter to the ARP compliance file (only one exists)
 ARPComplianceFile <- complianceFiles[complianceFiles$filename == 'compliance-arp.csv',]
 
+# Read the csv from source
 ARPCompliaceDF <- read.csv(paste0(bucketUrlBase,ARPComplianceFile$s3Path))

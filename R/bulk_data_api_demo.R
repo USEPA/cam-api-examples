24859fc70ec380dbce148b7d765c4331e7fd665f
kstetsyuk <stetsyuk.karina@epa.gov>
2022-09-13 16:43:49 -0400

    bug fix and add xml example

--- R/bulk_data_api_demo.R
+++ R/bulk_data_api_demo.R
@@ -32,7 +32,7 @@ print(csaprComplianceFiles)
 # Read the csvs from source and concatenate them together
 complianceData <- data.frame()
 for (s3Path in csaprComplianceFiles$s3Path){
-  print(paste0('S3 path: ',bucketUrlBase,s3Path))
+  print(paste0('Full path to file on S3: ',bucketUrlBase,s3Path))
   complianceData <- rbind(complianceData,read.csv(paste0(bucketUrlBase,s3Path)))
 }
 
@@ -48,36 +48,35 @@ print(dailyttQEmissionFiles)
 # Read the csvs from source and concatenate them together
 emissionData <- data.frame()
 for (s3Path in dailyttQEmissionFiles$s3Path){
-  print(paste0('S3 path: ',bucketUrlBase,s3Path))
+  print(paste0('Full path to file on S3: ',bucketUrlBase,s3Path))
   emissionData <- rbind(emissionData,read.csv(paste0(bucketUrlBase,s3Path)))
 }
 
-####### EDR Files #######
+####### XML Files #######
 
 library(utils)
 
-# reduce down to get the EDR files for Oklahoma
-edrFiles <- bulkFiles[bulkFiles$metadata$dataType=="EDR",]
-okEdrFiles <- edrFiles[edrFiles$metadata$stateCode=="OK",]
-print(okEdrFiles)
+# reduce down to get the XML files for Alabama
+xmlFiles <- bulkFiles[bulkFiles$metadata$dataType=="XML",]
+mpXmlEdrFiles <- xmlFiles[xmlFiles$metadata$dataSubType=="Monitoring Plan",]
+print(mpXmlEdrFiles)
 
-# get Riverside EDR files
-riversideEdrFiles <- edrFiles[edrFiles$metadata$description %like% "facility ID 4940",]
-read.csv(paste0(bucketUrlBase,s3Path))
+# get Alabama XML files
+alabamaXmlFiles <- mpXmlEdrFiles[mpXmlEdrFiles$metadata$stateCode =="AL",]
 
-# make directory for EDR files
+# make directory for XML files
 currentDirectory <- getwd()
-dir.create(paste0(currentDirectory,'RiversideEDRs'))
-riversideDirPath <- paste0(currentDirectory,'RiversideEDRs')
+dir.create(paste0(currentDirectory,'/AlabamaXML'))
+alabamaDirPath <- paste0(currentDirectory,'/AlabamaXML')
 
-# Download EDR files and place them in rierside directory
-for (i in 1:nrow(riversideEdrFiles)){
-  s3Path <- riversideEdrFiles$s3Path[i]
-  filename <- riversideEdrFiles$filename[i]
-  print(paste0('S3 path: ',bucketUrlBase,s3Path))
-  temp <- file.path(riversideDirPath,filename)
-  download.file(paste0(bucketUrlBase,s3Path),temp)
-  unzip(temp, exdir=riversideDirPath)
+# Download XML files and place them in rierside directory
+for (i in 1:nrow(alabamaXmlFiles)){
+  s3Path <- alabamaXmlFiles$s3Path[i]
+  filename <- alabamaXmlFiles$filename[i]
+  print(paste0('Full path to file on S3: ',bucketUrlBase,s3Path))
+  zipFile <- file.path(alabamaDirPath,filename)
+  download.file(paste0(bucketUrlBase,s3Path),zipFile)
+  unzip(zipFile, exdir=alabamaDirPath)
 }
 
 

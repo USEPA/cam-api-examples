40479442dfba244757b9d5fb13accfface50608b
kstetsyuk <stetsyuk.karina@epa.gov>
2022-09-14 08:19:58 -0400

    add streaming and mgmt endpoint examples

--- Python/facilities_data_demo.py
+++ Python/facilities_data_demo.py
@@ -1,37 +1,66 @@
 import requests
-import json
 import pandas as pd
 
-# This is an example of querying facility data.  
-# This is example also shows how to page through results.
+# These are examples of querying facility data.
+
+####### Streaming services API #######
+
+# If you have a lot of data to request, use the streaming services API to avoid
+# pagination and much quicker collection of the data you need.
 
 API_KEY = 'YOUR_API_KEY'
 
-# function for printing json neatly
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
+# api parameters for the streaming facility/attributes endpoint
+parameters = {
+    'api_key': API_KEY,
+    'year': 2020,
+    'unitFuelType': 'Coal|Natural Gas'
+}
+
+# making get request using the facilities/attributes endpoint
+streamingUrl = "https://api.epa.gov/easey/streaming-services/facilities/attributes"
+streamingResponse = requests.get(streamingUrl, params=parameters)
+
+# printing useful info from the response
+print("Status code: "+str(streamingResponse.status_code))
+print("Field Mappings: "+str(streamingResponse.headers['X-Field-Mappings']))
+# collecting data as a data frame
+streamingResponse_df = pd.DataFrame(streamingResponse.json())
+print(streamingResponse_df)
+
+####### Facilities mgmt API #######
+# This is example shows how to page through results.
+
+import math
 
 # api parameters for the facility attributes endpoint
 parameters = {
     'api_key': API_KEY,
     'year': 2020,
-    'facilityId': '3|7',
+    'unitFuelType': 'Coal|Natural Gas',
     'page': 1,
-    'perPage': 10
+    'perPage': 100
 }
 
 # making get request
-# Note: the x-total-count header is the total number of records in the response.  
+facilitiesUrl = "https://api.epa.gov/easey/facilities-mgmt/facilities/attributes"
+facilitiesResponse = requests.get(facilitiesUrl, params=parameters)
+
+# Note: the x-total-count header is the total number of records in the response.
 # You can use this to determine if you need to make another request to get all the records.
-# TODO: add pagination to this example
-response = requests.get("https://api.epa.gov/easey/facilities-mgmt/facilities/attributes", params=parameters)
-print("Status code: "+str(response.status_code))
+totalCount = facilitiesResponse.headers['X-Total-Count']
+print("Status code: "+str(facilitiesResponse.status_code))
+#print("Field Mappings: "+str(response.headers['X-Field-Mappings']))
+print("Total Count: "+str(totalCount))
 
-# printing in json format
-jprint(response.json())
+# print the first page of the data
+facilitiesResponse_df = pd.DataFrame(facilitiesResponse.json())
+print(facilitiesResponse_df)
 
-# convert to a data frame and print first 5 rows
-response_df = pd.DataFrame(response.json())
-print(response_df)
+# print other pages of the data if it's available
+if math.ceil(int(totalCount)/int(parameters['perPage'])) > 1:
+    for x in range(math.ceil(int(totalCount)/int(parameters['perPage']))-1):
+        parameters['page'] += 1
+        facilitiesResponse = requests.get(facilitiesUrl, params=parameters)
+        facilitiesResponse_df = pd.DataFrame(facilitiesResponse.json())
+        print(facilitiesResponse_df)
--- R/facility_data_demo.R
+++ R/facility_data_demo.R
@@ -6,7 +6,29 @@ library(jsonlite)
 apiUrlBase <- "https://api.epa.gov/easey"
 apiKEY <- "YOUR_API_KEY"
 
-####### Emissions mgmt API #######
+####### Streaming services API #######
+
+# If you have a lot of data to request, use the streaming services API to avoid
+# pagination and much quicker collection of the data you need.
+
+# streaming facility attributes endpoint url
+facilitiesUrl <- paste0(apiUrlBase,"/streaming-services/facilities/attributes?API_KEY=",apiKEY)
+
+# define query parameters
+query <- list(year="2020",
+              stateCode=paste0(c("IN","MI"), collapse = '|'),
+              unitFuelType=paste0(c("Coal","Natural Gas"), collapse = '|'))
+
+# API GET request
+res = GET(facilitiesUrl, query = query)
+
+# convert response to a data frame
+facilitiesData <- fromJSON(rawToChar(res$content))
+
+# print head of dataframe
+print(head(facilitiesData))
+
+####### Facilities mgmt API #######
 
 # facility attributes endpoint url
 facilitiesPageUrl <- paste0(apiUrlBase,"/facilities-mgmt/facilities/attributes?API_KEY=",apiKEY)
@@ -31,25 +53,3 @@ print(fieldMapping)
 # convert response to a data frame
 facilityDataPage <- fromJSON(rawToChar(res$content))
 print(facilityDataPage)
-
-####### Streaming services API #######
-
-# If you have a lot of data to request, use the streaming services API to avoid
-# pagination and much quicker collection of the data you need.
-
-# streaming facility attributes endpoint url
-facilitiesUrl <- paste0(apiUrlBase,"/streaming-services/facilities/attributes?API_KEY=",apiKEY)
-
-# define query parameters
-query <- list(year="2020",
-              stateCode=paste0(c("IN","MI"), collapse = '|'),
-              unitFuelType=paste0(c("Coal","Natural Gas"), collapse = '|'))
-
-# API GET request
-res = GET(facilitiesUrl, query = query)
-
-# convert response to a data frame
-facilitiesData <- fromJSON(rawToChar(res$content))
-
-# print head of dataframe
-print(head(facilitiesData))

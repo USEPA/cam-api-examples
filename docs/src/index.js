8e56767a667517be8b3f05644e6a170f0635b37d
kstetsyuk <stetsyuk.karina@epa.gov>
2023-08-04 11:32:27 -0400

    imporve page speed

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,8 +1,8 @@
 
+var API_KEY = process.env.API_KEY;
 
-(async () => {
-  var API_KEY = process.env.API_KEY;
-  const monPlanConfigResponse = await fetch('https://api.epa.gov/easey/beta/monitor-plan-mgmt/configurations?orisCodes=3',
+async function monPlanConfigCall() {
+  await fetch('https://api.epa.gov/easey/beta/monitor-plan-mgmt/configurations?orisCodes=3',
     {
       headers: {
         "x-api-key": process.env.API_KEY
@@ -13,7 +13,11 @@
   const monPlanData = await monPlanConfigResponse.json();
   const monPlanElem = document.getElementById("monitoring-plan-config-response");
   monPlanElem.innerHTML = '<code class="language-json">'+JSON.stringify(monPlanData, null, 4);+'</code>'
-  const monPlanId = monPlanData[5].id;
+}
+monPlanConfigCall();
+
+async function emissionsExportCall() {
+  const monPlanId = 'TWCORNEL5-488E42008B434177BC7D7BFF138D18EF';
   document.getElementById("monitoring-plan-id").innerHTML = monPlanId;
   document.getElementById("emissions-export-url").innerHTML = 'https://api.epa.gov/easey/beta/emissions-mgmt/emissions/export?monitorPlanId='+monPlanId+'&year=2022&quarter=4';
   
@@ -28,8 +32,11 @@
   const emissionsExportData = await emissionsExportResponse.json();
   const emissionsExportElem = document.getElementById("emissions-export-response");
   emissionsExportElem.innerHTML = '<code class="language-json">'+JSON.stringify(emissionsExportData, null, 4);+'</code>'
+}
+emissionsExportCall();
 
-  const locId = monPlanData[5].locations[0].id;
+async function locationAttributeCall() {
+  const locId = '11';
   document.getElementById("location-id").innerHTML = locId;
   document.getElementById("location-attributes-url").innerHTML = 'https://api.epa.gov/easey/beta/monitor-plan-mgmt/locations/'+locId+'/attributes';
   
@@ -44,4 +51,6 @@
   const locationAttributeData = await locationAttributeResponse.json();
   const locationAttributeElem = document.getElementById("location-attributes-response");
   locationAttributeElem.innerHTML = '<code class="language-json">'+JSON.stringify(locationAttributeData, null, 4);+'</code>'
-})();
+}
+locationAttributeCall();
+
